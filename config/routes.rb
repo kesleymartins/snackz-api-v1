e9b79a20eef57d2f95a736b2354f1fb9c228fcf5
kesleymartins <kesleymartins@vivaldi.net>
2023-06-07 11:04:17 -0300

    setup app endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :orders
+    end
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'orders#index'
 end

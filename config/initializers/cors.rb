3a1b4a041db347d883f385e1b5a50ef059251fc6
kesleymartins <kesleymartins@vivaldi.net>
2023-06-10 12:18:48 -0300

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,10 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-
-    resource '*',
-      headers: :any,
-      methods: %i[ get :post :delete ]
+    origins 'chrome-extension://jabjjdjjplbmdkfpgmdgmgcbkefodbio'
+    
+    resource '*', 
+      headers: :any, 
+      methods: [:get, :post, :put, :patch, :delete, :options, :head], 
+      credentials: true
   end
 end

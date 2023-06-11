161db7b27df8e61de3fbc54059d42e6e9c7dfcdc
kesleymartins <kesleymartins@vivaldi.net>
2023-06-11 19:32:23 -0300

    add scope to filter order descriptions

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,5 @@ class Order < ApplicationRecord
 
   scope :today, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
   scope :results_for, -> (column) { group(column).count }
+  scope :descriptions, -> { select([:id, :name, :description]) }
 end

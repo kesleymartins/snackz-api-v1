f85a493c353d872831ed4cb419f4b3fc8e1f71df
kesleymartins <kesleymartins@vivaldi.net>
2023-06-18 16:23:56 -0300

    update scopes order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   validates :food, presence: true
 
-  scope :today, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
   scope :results_for, -> (column) { group(column).count }
   scope :descriptions, -> { select([:id, :name, :description]) }
+  scope :today, -> { where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day) }
 end
